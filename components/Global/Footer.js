92855dc716d13faccde61d817cb34a3e06679c6f
alejandrogsk <alejandrogsk9900@gmail.com>
2022-05-20 10:26:47 -0300

    Footer Fixed

--- components/Global/Footer.js
+++ components/Global/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='mx-4 sm:mx-8  md:mx-12 mx:mx-20 grid grid-cols-1 md:grid-cols-2 content-between	'>
+    <footer className='py-4 md:py-8 mx-4 sm:mx-8  md:mx-12 mx:mx-20 grid grid-cols-1 md:grid-cols-2 content-between	'>
       <ul className="flex flex-row flex-wrap mb-4 md:mb-0	mx-auto md:mx-0">
                     <a className="mr-4 cursor-pointer hover:text-ui-orange-main ease-in-out duration-300">
                         Home
